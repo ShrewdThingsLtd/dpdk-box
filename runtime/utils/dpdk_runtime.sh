36b519dbee97043d856f638aaa3368ce6e867848
Yotam Ramon <yotam@ethernitynet.com>
2019-01-01 23:20:40 -0500

    reposition remote dpdk install

--- runtime/utils/dpdk_runtime.sh
+++ runtime/utils/dpdk_runtime.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-dpdk_remote_install() {
-
-	local remote_dir="${TGT_SRC_DIR}"
-##################
-local remote_cmd="\
-export SRC_DIR=${TGT_SRC_DIR};\
-export DPDK_DIR=${TGT_SRC_DIR}/dpdk;\
-export DPDK_REPO=${DPDK_REPO};\
-export DPDK_VERSION=${DPDK_VERSION};\
-export DPDK_TARGET=${DPDK_TARGET};\
-source ${TGT_SRC_DIR}/dpdk-box/app/utils/exec_utils.sh;\
-source ${TGT_SRC_DIR}/dpdk-box/app/utils/git_utils.sh;\
-source ${TGT_SRC_DIR}/dpdk-box/app/utils/dpdk_utils.sh;\
-source ${TGT_SRC_DIR}/dpdk-box/runtime/utils/dpdk_runtime.sh;\
-yum -y install numactl-devel;\
-dpdk_clone;\
-dpdk_kni_build_disable;\
-dpdk_build"
-##################
-	exec_tgt "${remote_dir}" "${remote_cmd}"
-}
-
 dpdk_igb_uio_install_module() {
 
 	local remote_dir="${TGT_SRC_DIR}"
